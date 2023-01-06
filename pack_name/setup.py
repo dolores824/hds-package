e41d8ef04c361ef9f8f5b92272e23f22b2860412
dolores824 <dolores.tan@qq.com>
2023-01-06 13:19:46 +0000

    Commit

--- pack_name/setup.py
+++ pack_name/setup.py
@@ -7,6 +7,13 @@ setup(
     description="<pack desc>",
     name="<pack_name>",
     packages=find_packages(include=["<pack_name>", "<pack_name>.*"]),
+    
+    #install_requires=[
+    #    'pandas',            # any versions of pandas
+    #    'matplotlib>=2.2.1'
+    #],
+    #python_requires='>=3.0',
+    
     version="0.1.0",
 )
 # Run this to install the package locally in editable mode
