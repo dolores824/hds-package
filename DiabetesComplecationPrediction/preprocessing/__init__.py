96c4d8a1ddce3a0125e4fd7657439c5111c39a75
dolores824 <dolores.tan@qq.com>
2023-01-13 17:08:07 +0000

    Add function for output the feature list; debug

--- DiabetesComplecationPrediction/preprocessing/__init__.py
+++ DiabetesComplecationPrediction/preprocessing/__init__.py
@@ -2,14 +2,14 @@
 This module is for data cleaning and featurization.
 """
 
-from .correlation import cor_heatmap, most_correlated, view_correlations, most_related_heatmap
+from .correlation import cor_heatmap, Feature
 from .encode import encode_categ
 from .load import load_csv
 from .missing import DropNaN, Del_Feature, normal_impute, interpolate_impute, knn_impute, rf_impute
 from .skew import skewness, box_cox
 
 __all__ = [
-    'cor_heatmap', 'most_correlated', 'view_correlations', 'most_related_heatmap',
+    'cor_heatmap', 'Feature',
     'encode_categ',
     'load_csv',
     'DropNaN', 'Del_Feature', 'normal_impute', 'interpolate_impute', 'knn_impute', 'rf_impute',
