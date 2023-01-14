5ba5d347bdb7601484dedaa31822feb6fcd2dde3
Stephanie <yangnistephanie@gmail.com>
2023-01-14 06:16:34 +0000

    Add some description of the functions.

--- DiabetesComplecationPrediction/information.py
+++ DiabetesComplecationPrediction/information.py
@@ -3,25 +3,18 @@ This module contains some functions to collect the input information and illustr
 """
 from DiabetesComplecationPrediction.trained_model import cvd_risk_prediction, IgAN_risk_prediction
 from DiabetesComplecationPrediction.error import *
-import pandas as pd
-
-def df_to_dict(df):
-    """
-    This function changes the format of patient's information from dataframe to dictionary.
-
-    Args:
-        df (DataFrame): information inputed by the patient
-
-    Returns:
-        A dictionary that has the information inputed by the patients:
-            -keys of the dictionary are the features
-            -values are the status of the corresponding features
-    """
-    dict = pd.Dataframe(df, 'index')
-    del dict['Features']
-    return dict
 
 def cvd_checker_csv(input_dataset, classification_method = ['SVM', 'Random Forest']):
+    """
+    This function takes a dataframe as input and returns the prediction using chosen classification method.
+
+    Args:
+        input_dataset (DataFrame): the information inputted by the patient who want to get a general prediction
+        classification_method (string): the name of the classification method (either SVM or random forest)
+
+    Returns:
+        Two sentences that illustrate the prediction of the cvd risk result and accuracy
+    """
     info = input_dataset.to_dict('index')['Values']
     if classification_method == 'SVM':
         return cvd_risk_prediction(info, 'SVM')
@@ -31,7 +24,17 @@ def cvd_checker_csv(input_dataset, classification_method = ['SVM', 'Random Fores
         raise DiaCcsPredError('Please choose a valid model type: SVM or Random Forest.')
     
 def IgAN_checker_csv(input_dataset, classification_method = ['SVM', 'Random Forest']):
-    info = input_dataset.to_dict()
+    """
+    This function takes a dataframe as input and returns the prediction using chosen classification method.
+
+    Args:
+        input_dataset (DataFrame): the information inputted by the patient who want to get a general prediction
+        classification_method (string): the name of the classification method (either SVM or random forest)
+
+    Returns:
+        Two sentences that illustrate the prediction of the IgAN risk result and accuracy
+    """
+    info = input_dataset.to_dict('index')['Values']
     if classification_method == 'SVM':
         return IgAN_risk_prediction(info, 'SVM')
     elif classification_method == 'Random Forest':
